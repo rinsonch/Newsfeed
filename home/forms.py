b43cfe51b08ade06ca6f58f3e91c74e153b5b5e3
akshay <you@example.com>
2017-11-20 18:13:23 +0530

    Deploying after correction date

--- home/forms.py
+++ home/forms.py
@@ -6,7 +6,7 @@ class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     username=forms.EmailField(max_length=30)
-    dob=forms.DateField(required=False,widget=forms.D(attrs={
+    dob=forms.DateField(required=False,widget=forms.DateInput(attrs={
         'class':'datepicker', 'data-min': '1960-01-01',
         'data-max': '2000-12-12'}))
     profpic=forms.ImageField(required=False)
