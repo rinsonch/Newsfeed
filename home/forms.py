9e055cce1ef502c805ec2be51f38bc96db8e5977
akshay <you@example.com>
2017-11-20 17:48:35 +0530

    Deploying after correction

--- home/forms.py
+++ home/forms.py
@@ -6,11 +6,15 @@ class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     username=forms.EmailField(max_length=30)
-    dob=forms.DateField(required=False)
+    dob=forms.DateField(required=False,widget=forms.DateInput(attrs={
+        'class':'datepicker', 'data-min': '1960-01-01',
+        'data-max': '2000-12-12'}))
     profpic=forms.ImageField(required=False)
 
 
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username','email', 'password1', 'password2', 'dob','profpic' )
+
+
 
